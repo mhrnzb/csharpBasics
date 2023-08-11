264e738f6441e224777cb711d09265d947e50c44
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-12 01:57:21 +0330

    update consoleApp

--- session2/ConsoleApp1/ConsoleApp1/Program.cs
+++ session2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string Name = "name 1";
+            //string Name = "name 2";    // it means you can not have variable with same name
+            string nAme = "name 3";    // CSharp is case sensitive.
+            Console.WriteLine(Name);
+            Console.WriteLine(nAme);
+            Console.ReadKey();
+
         }
+    }
+
+    public class test2
+    {
+        //we cant see name here.
+        // we can access the variable only in the specified block.
+        string Name = "sara ";
+
+        //here you can use  Name variable .
     }
 }
