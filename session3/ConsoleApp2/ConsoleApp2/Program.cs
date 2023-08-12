a179c853f36b4cdde78da5305234fe2511025049
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-12 18:17:46 +0330

    learning how to convert DataTypes

---
+++ session3/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            string test = Convert.ToString(a);
+
+            
+
+            string num1 = "123456";
+
+            int num = Convert.ToInt32(num1);
+
+            num = num + 123;
+            // num++;
+            // num--;
+            //num +=3;
+
+            bool isActive = Convert.ToBoolean("True");
+            
+            Console.WriteLine(test);
+            Console.WriteLine(num);
+            Console.ReadKey();
+
+
+        }
+    }
+}
