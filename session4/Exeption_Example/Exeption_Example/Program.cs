b61b2341ddeed526a307d24b6940f9ce844c4456
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-12 19:47:47 +0330

    learning try catch and error handeling

--- session4/Exeption_Example/Exeption_Example/Program.cs
+++ session4/Exeption_Example/Exeption_Example/Program.cs
@@ -10,16 +10,39 @@ namespace Exeption_Example
     {
         static void Main(string[] args)
         {
-            int num1;
-            int num2;
+            int num1 = 0;
+            int num2 = 0;
 
+
+
+            try
+            {
             Console.WriteLine("please enter number 1 :");
             num1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("please enter number 2 :");
             num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Sum is" + num1 + num2);
+            }
+
+            catch(FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("please enter just number.");
+                Console.ResetColor();
+            }
+            catch 
+            {
+                Console.WriteLine("un expected error");
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            
+
+            Console.WriteLine("Sum is" + (num1 + num2));
+
             Console.ReadKey();
         }
     }
