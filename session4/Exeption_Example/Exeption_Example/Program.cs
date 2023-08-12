db7e66aa05e27dafd9481ed92eae33c0c639c83c
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-12 19:14:26 +0330

    create file

---
+++ session4/Exeption_Example/Exeption_Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exeption_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine("please enter number 1 :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter number 2 :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum is" + num1 + num2);
+            Console.ReadKey();
+        }
+    }
+}
