a449bee987a3928f97e69e3777f40e311f250026
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-12 17:43:52 +0330

    coding1

--- session3/ConsoleApp1/ConsoleApp1/Program.cs
+++ session3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,19 @@ namespace ConsoleApp1
         {   // Data types
             int a;
             a = 10;
+
+
+            int b, c;
+            b = 20;
+            c= a + b;
+
+
+            string name = "Mehrnaz ";
+            string FamilyName = "Bagheri";
+            string FullName = name + FamilyName;
+            string age = "20";
+
+
             Console.WriteLine(a);
             Console.ReadKey();
 
