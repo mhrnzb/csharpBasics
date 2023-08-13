039b7b6778e04cbe133ddd6628a100dd9a637e6c
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-13 20:56:37 +0330

    try catch used here

---
+++ session6/Person_Example/Person_Example/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+            try
+            {
+            Console.WriteLine("please enter person number : ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch
+            {
+                Console.WriteLine("please enter just number ...");
+            }
+
+
+
+
+
+        }
+    }
+}
