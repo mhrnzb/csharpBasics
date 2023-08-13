1991cb7410c32543e25a07c57bb278f21c80009d
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-13 20:50:41 +0330

    array file and for

--- session6/Array_Example/Array_Example/Program.cs
+++ session6/Array_Example/Array_Example/Program.cs
@@ -10,7 +10,19 @@ namespace Array_Example
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 1, 2, 3 , 4 };
 
+            int number = numbers[2];
+
+            string[] Names = {"name1" , "name2" , "name3" };
+
+            for (int i=0;  i<3; i++)
+            {
+                Console.WriteLine(Names[i]);
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
