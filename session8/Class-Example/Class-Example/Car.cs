062dc95789e34a48c156e2bcb05d04d0c23644ee
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-14 00:59:47 +0330

    add a comment about cunstractor

--- session8/Class-Example/Class-Example/Car.cs
+++ session8/Class-Example/Class-Example/Car.cs
@@ -8,5 +8,14 @@ namespace Class_Example
 {
     internal class Car
     {
+        public string carName;
+        public int carSpeed;
+        public string carModel;
+        // a method with public access modifier and same name with class name , is a CONSTRUCTOR.
+
+        public Car()
+        {
+            //LOGIC
+        }
     }
 }
