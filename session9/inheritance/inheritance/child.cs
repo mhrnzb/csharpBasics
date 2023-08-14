449be3e3dac9eab64292507edbf005e179d959bc
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-14 22:21:18 +0330

    Make istances

---
+++ session9/inheritance/inheritance/child.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class child
+    {
+
+        public static int multiple(int a, int b)
+        {
+            return a * b;
+
+        }
+    }
+}
