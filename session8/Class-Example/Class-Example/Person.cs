f9e7e964eaee68d92ab1bccc49d6cb7983a420ef
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-14 14:44:04 +0330

    person class added

---
+++ session8/Class-Example/Class-Example/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Example
+{
+    internal class Person
+    {
+
+        public string Name;
+        public string Family;
+        public string website;
+
+
+
+        public Person(string name, string family, string website)
+        {
+
+        }
+    }
+}
