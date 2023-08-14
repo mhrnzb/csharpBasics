dd430a1092f351bcb2917f10f2557895b66c3446
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-14 22:19:24 +0330

    learning inheritance

---
+++ session9/inheritance/inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(parent.sum(5, 6));
+            Console.WriteLine(parent.minus(10, 8));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ session9/inheritance/inheritance/parent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class parent
+    {
+        public static int sum(int a , int b)
+        {
+            return a + b;
+        }
+
+        public static int minus(int a , int b)
+        {
+            return a - b;
+
+        }
+    }
+}
