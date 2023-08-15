42d8a25b0cc486341f775d158b31c21560fe9575
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-15 22:58:52 +0330

    first windows form application

---
+++ session 13/FirstAPP/FirstAPP/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FirstAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSayHello = new System.Windows.Forms.Button();
+            this.lblHello = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "enter your name :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(177, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(222, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnSayHello
+            // 
+            this.btnSayHello.Location = new System.Drawing.Point(211, 69);
+            this.btnSayHello.Name = "btnSayHello";
+            this.btnSayHello.Size = new System.Drawing.Size(115, 23);
+            this.btnSayHello.TabIndex = 2;
+            this.btnSayHello.Text = "Say Hello";
+            this.btnSayHello.UseVisualStyleBackColor = true;
+            this.btnSayHello.Click += new System.EventHandler(this.btnSayHello_Click);
+            // 
+            // lblHello
+            // 
+            this.lblHello.AutoSize = true;
+            this.lblHello.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblHello.Location = new System.Drawing.Point(174, 122);
+            this.lblHello.Name = "lblHello";
+            this.lblHello.Size = new System.Drawing.Size(0, 16);
+            this.lblHello.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 186);
+            this.Controls.Add(this.lblHello);
+            this.Controls.Add(this.btnSayHello);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello World ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSayHello;
+        private System.Windows.Forms.Label lblHello;
+    }
+}
+
